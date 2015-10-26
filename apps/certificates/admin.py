8c0b857b288c76aa21f31042f55819878b20da7e
Alan Viars <alan.c.viars@gmail.com>
2015-10-26 16:37:52 -0400

    Customize Admin.

--- apps/certificates/admin.py
+++ apps/certificates/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 from models import ( DomainBoundCertificate, TrustAnchorCertificate,
                     CertificateRevocationList, AnchorCertificateRevocationList)
 
 
+
+class TreeViewAnchorCertificate(TrustAnchorCertificate):
+    class Meta:
+        proxy = True
+        verbose_name = "Tree View of Anchor"
+
+
 class DomainBoundCertificateAdmin(admin.ModelAdmin):
     
-    list_display = ('domain', 'status', 'verified','serial_number',
+    list_display = ('common_name', 'verified','serial_number',
                     'organization', 'creation_date', 'expiration_date')
     
-    search_fields = ('domain','status', 'verified','serial_number',
+    search_fields = ('common_name','status', 'verified','serial_number',
                      'organization', 'creation_date', 'expiration_date')
     
 admin.site.register(DomainBoundCertificate, DomainBoundCertificateAdmin)
@@ -16,13 +24,31 @@ admin.site.register(DomainBoundCertificate, DomainBoundCertificateAdmin)
 
 class TrustAnchorCertificateAdmin(admin.ModelAdmin):
     
-    list_display = ('domain','status', 'verified', 'serial_number',
+    #def queryset(self, request):
+    #    return self.model.objects.filter(user = request.user)
+
+    list_display = ('domain','parent', 'status', 'verified', 'serial_number',
                     'organization', 'creation_date', 'expiration_date')
     
-    search_fields = ('domain', 'status','verified', 'serial_number',
+    search_fields = ( 'common_name', 'domain', 'email',  'parent__common_name', 'status','verified', 'serial_number',
                      'organization', 'creation_date', 'expiration_date')
+
     
 admin.site.register(TrustAnchorCertificate, TrustAnchorCertificateAdmin)
+
+
+
+
+
+class TreeViewAdmin(MPTTModelAdmin):
+                   
+    search_fields = ('common_name','parent',)
+    
+    #def queryset(self, request):
+    #    return self.model.objects.filter(user = request.user)
+    
+admin.site.register(TreeViewAnchorCertificate, TreeViewAdmin)
+
 
 
 class CertificateRevocationListAdmin(admin.ModelAdmin):
